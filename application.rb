114161cf29519863d7ebd7ea6a492319597d745d
Andrew Jones <joneseyit@protonmail.com>
2018-11-17 10:51:37 -0500

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,15 @@
+# class Application
+#
+#   def call(env)
+#     resp = Rack::Response.new
+#     resp.write "Hello, World"
+#     resp.finish
+#   end
 class Application
-
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, Dude.  Hello, my name is Jones.  Dominate yourself, dominate the world."
     resp.finish
   end
 
 end
-
